a907457e5ef3e5f3b503c32dc87e1e9723b72cde
musyafa619 <musyafaarts@gmail.com>
2026-01-18 16:33:43 +0700

    Add detail barang ui

--- src/pages/deskripsibarang/index.tsx
+++ src/pages/deskripsibarang/index.tsx
@@ -1,34 +1,33 @@
-import styles from "./style.module.css";
-import LaptopImage from "../../assets/images/laptop.png";
+import styles from './style.module.css';
+import LaptopImage from '../../assets/images/laptop.png';
 
-export const DetailBarang = ({ onClose }) => {
+interface DetailBarangProps {
+  onClose: () => void;
+}
+
+export const DetailBarang = ({ onClose }: DetailBarangProps) => {
   return (
     <div className={styles.overlay}>
       <div className={styles.card}>
-        
         {/* BUTTON CLOSE */}
         <button className={styles.closeBtn} onClick={onClose}>
           &times;
         </button>
 
-        <img
-          src={LaptopImage}
-          alt="Laptop Acer"
-          className={styles.image}
-        />
+        <img src={LaptopImage} alt="Laptop Acer" className={styles.image} />
         <div className={styles.content}>
           <h3 className={styles.title}>Laptop Acer</h3>
           <p className={styles.subtitle}>Acer Swift Lite 14 Air</p>
 
           <p className={styles.description}>
-            Deskripsi : Acer Swift Lite 14 Air Edition adalah laptop ultra-ringan
-            dengan bobot hanya sekitar 999 gram, dirancang untuk pengguna yang
-            mengutamakan mobilitas tinggi tanpa mengorbankan performa.
-            Laptop ini ditenagai oleh Intel Core Ultra 5 115U,
+            Deskripsi : Acer Swift Lite 14 Air Edition adalah laptop
+            ultra-ringan dengan bobot hanya sekitar 999 gram, dirancang untuk
+            pengguna yang mengutamakan mobilitas tinggi tanpa mengorbankan
+            performa. Laptop ini ditenagai oleh Intel Core Ultra 5 115U,
             prosesor generasi terbaru yang didukung teknologi Intel AI Boost
             untuk menunjang performa harian serta fitur kecerdasan buatan (AI).
           </p>
-          
+
           <p className={styles.color}>
             <strong>Warna :</strong> Hitam
           </p>
